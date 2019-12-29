c7628d06d98a46e56ebcd68b5496970e0a4474e5
Muhao Guo <muhaoguo@MuhaodeMacBook-Pro.local>
2019-12-29 01:14:03 -0500

    Updates

---
+++ precache-manifest.006176f7e87c0fd2905108d2d0656e1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b059092ef3bfe0ab18f7dc7a588de88b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f0b438522b1c84bc690e",
+    "url": "/static/css/main.44ab47d7.chunk.css"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/static/js/2.8d7c7b1f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8d7c7b1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0b438522b1c84bc690e",
+    "url": "/static/js/main.66c7b8ca.chunk.js"
+  },
+  {
+    "revision": "aa6fed8716c2be098a58",
+    "url": "/static/js/runtime-main.55a556ca.js"
+  }
+]);
